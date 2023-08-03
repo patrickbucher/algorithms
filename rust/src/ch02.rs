8f37ca330b82a45186fabc6cd15fcc9b34fd4ad4
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-03 22:36:01 +0200

    Rust: binary addition

--- rust/src/ch02.rs
+++ rust/src/ch02.rs
@@ -32,13 +32,92 @@ pub fn linear_search<T: Eq>(haystack: &Vec<T>, needle: T) -> Option<usize> {
     return None;
 }
 
+pub fn add_binary(xs: &Vec<u8>, ys: &Vec<u8>) -> Vec<u8> {
+    let m = xs.len();
+    let n = ys.len();
+    if m == 0 {
+        return ys.clone();
+    }
+    if n == 0 {
+        return xs.clone();
+    }
+    if m != n {
+        return Vec::new();
+    }
+    let mut zs: Vec<u8> = Vec::with_capacity(m + 1);
+    for i in 0..=m {
+        zs.push(0);
+    }
+    for i in 0..m {
+        let e = xs[i] + ys[i] + zs[i];
+        if e == 0 || e == 1 {
+            zs[i] = e;
+        } else if e == 2 {
+            zs[i] = 0;
+            zs[i + 1] = 1;
+        } else if e == 3 {
+            zs[i] = 1;
+            zs[i + 1] = 1;
+        }
+    }
+    return zs;
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::ch02::add_binary;
     use crate::ch02::insertion_sort;
     use crate::ch02::linear_search;
     use crate::sorting::equal;
     use crate::sorting::is_sorted_asc;
     use crate::sorting::random_vec;
+
+    #[test]
+    fn test_add_empty_binaries() {
+        assert!(equal(&add_binary(&Vec::new(), &Vec::new()), &Vec::new()));
+    }
+
+    #[test]
+    fn test_add_to_empty_binary() {
+        assert!(equal(&add_binary(&Vec::new(), &vec![0]), &vec![0]));
+        assert!(equal(&add_binary(&vec![0], &Vec::new()), &vec![0]));
+    }
+
+    #[test]
+    fn test_add_of_different_size() {
+        assert!(equal(&add_binary(&vec![0, 1], &vec![1]), &Vec::new()));
+    }
+
+    #[test]
+    fn test_one_plus_one_is_two() {
+        assert!(equal(&add_binary(&vec![1], &vec![1]), &vec![0, 1]));
+    }
+
+    #[test]
+    fn test_one_plus_two_is_three() {
+        assert!(equal(&add_binary(&vec![1, 0], &vec![0, 1]), &vec![1, 1, 0]));
+    }
+
+    #[test]
+    fn test_two_plus_two_is_four() {
+        assert!(equal(&add_binary(&vec![0, 1], &vec![0, 1]), &vec![0, 0, 1]));
+    }
+
+    #[test]
+    fn test_ten_plus_five_is_fifteen() {
+        assert!(equal(
+            &add_binary(&vec![0, 1, 0, 1], &vec![1, 0, 1, 0]),
+            &vec![1, 1, 1, 1, 0]
+        ));
+    }
+
+    #[test]
+    fn test_eleven_plus_five_is_sixteen() {
+        assert!(equal(
+            &add_binary(&vec![1, 1, 0, 1], &vec![1, 0, 1, 0]),
+            &vec![0, 0, 0, 0, 1]
+        ));
+    }
 
     #[test]
     fn test_linear_search_empty() {
