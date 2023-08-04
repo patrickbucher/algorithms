a5be664d854d056be0524eac75fa136bfd7726aa
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-04 14:37:56 +0200

    Erlang: generate lists of random numbers

--- erlang/ch02/sorting.erl
+++ erlang/ch02/sorting.erl
@@ -1,8 +1,18 @@
 -module(sorting).
--export([equal/2]).
+-export([equal/2, random_list/3]).
 
 equal([], []) -> true;
 equal([_], []) -> false;
 equal([], [_]) -> false;
 equal([HL|TL], [HR|TR]) ->
     HL =:= HR andalso equal(TL, TR).
+
+random_list(0, _, _) -> [];
+random_list(_, Min, Max) when Min >= Max -> [];
+random_list(N, Min, Max) ->
+    random_list(N, Min, Max, []).
+
+random_list(0, _, _, Acc) -> Acc;
+random_list(N, Min, Max, Acc) ->
+    Random = rand:uniform(Max - Min - 1) + Min,
+    random_list(N - 1, Min, Max, [Random|Acc]).
--- erlang/ch02/sorting_tests.erl
+++ erlang/ch02/sorting_tests.erl
@@ -8,3 +8,9 @@ equal_test() ->
     ?assert(sorting:equal([1], [1])),
     ?assert(sorting:equal([1, 2], [1, 2])),
     ?assertNot(sorting:equal([1, 3], [1, 2])).
+
+random_list_test() ->
+    ?assertEqual(sorting:random_list(0, 1, 10), []),
+    ?assertEqual(sorting:random_list(5, 10, 1), []),
+    List = sorting:random_list(1000, 10, 20),
+    ?assert(lists:all(fun(Element) -> (Element >= 10 andalso Element < 20) end, List)).
