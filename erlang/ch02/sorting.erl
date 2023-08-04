78c15a255e0571990c46c036a5167fe83c77f52f
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-04 17:09:55 +0200

    Erlang: asc/desc sorting

--- erlang/ch02/sorting.erl
+++ erlang/ch02/sorting.erl
@@ -1,5 +1,5 @@
 -module(sorting).
--export([equal/2, random_list/3, is_sorted/1]).
+-export([equal/2, random_list/3, is_sorted/2, is_sorted_asc/1, is_sorted_desc/1]).
 
 equal([], []) -> true;
 equal([_], []) -> false;
@@ -17,10 +17,14 @@ random_list(N, Min, Max, Acc) ->
     Random = rand:uniform(Max - Min - 1) + Min,
     random_list(N - 1, Min, Max, [Random|Acc]).
 
-is_sorted([]) -> true;
-is_sorted([_]) -> true;
-is_sorted([H|T]) -> is_sorted(H, T).
+is_sorted([], _) -> true;
+is_sorted([_], _) -> true;
+is_sorted([H|T], Cmp) -> is_sorted(H, T, Cmp).
 
-is_sorted(_, []) -> true;
-is_sorted(Last, [H]) -> Last =< H;
-is_sorted(Last, [H|T]) -> Last =< H andalso is_sorted(H, T).
+is_sorted(_, [], _) -> true;
+is_sorted(Last, [H], Cmp) -> Cmp(Last, H);
+is_sorted(Last, [H|T], Cmp) -> Cmp(Last, H) andalso is_sorted(H, T, Cmp).
+
+is_sorted_asc(L) -> is_sorted(L, fun(A, B) -> A =< B end).
+
+is_sorted_desc(L) -> is_sorted(L, fun(A, B) -> A >= B end).
--- erlang/ch02/sorting_tests.erl
+++ erlang/ch02/sorting_tests.erl
@@ -16,7 +16,33 @@ random_list_test() ->
     ?assert(lists:all(fun(Element) -> (Element >= 10 andalso Element < 20) end, List)).
 
 is_sorted_test() ->
-    ?assert(sorting:is_sorted([])),
-    ?assert(sorting:is_sorted([1])),
-    ?assert(sorting:is_sorted([1, 2])),
-    ?assert(sorting:is_sorted([1, 2, 3, 4, 5])).
+    Asc = fun(A, B) -> A =< B end,
+    Desc = fun(A, B) -> A >= B end,
+    ?assert(sorting:is_sorted([], Asc)),
+    ?assert(sorting:is_sorted([], Desc)),
+    ?assert(sorting:is_sorted([1], Asc)),
+    ?assert(sorting:is_sorted([1], Desc)),
+    ?assert(sorting:is_sorted([1, 2], Asc)),
+    ?assertNot(sorting:is_sorted([1, 2], Desc)),
+    ?assertNot(sorting:is_sorted([2, 1], Asc)),
+    ?assert(sorting:is_sorted([2, 1], Desc)),
+    ?assert(sorting:is_sorted([1, 2, 3, 4, 5], Asc)),
+    ?assertNot(sorting:is_sorted([1, 2, 3, 4, 5], Desc)),
+    ?assertNot(sorting:is_sorted([5, 4, 3, 2, 1], Asc)),
+    ?assert(sorting:is_sorted([5, 4, 3, 2, 1], Desc)).
+
+is_sorted_asc_test() ->
+    ?assert(sorting:is_sorted_asc([])),
+    ?assert(sorting:is_sorted_asc([1])),
+    ?assert(sorting:is_sorted_asc([1, 2])),
+    ?assertNot(sorting:is_sorted_asc([2, 1])),
+    ?assert(sorting:is_sorted_asc([1, 2, 3, 4, 5])),
+    ?assertNot(sorting:is_sorted_asc([5, 4, 3, 2, 1])).
+
+is_sorted_desc_test() ->
+    ?assert(sorting:is_sorted_desc([])),
+    ?assert(sorting:is_sorted_desc([1])),
+    ?assertNot(sorting:is_sorted_desc([1, 2])),
+    ?assert(sorting:is_sorted_desc([2, 1])),
+    ?assertNot(sorting:is_sorted_desc([1, 2, 3, 4, 5])),
+    ?assert(sorting:is_sorted_desc([5, 4, 3, 2, 1])).
