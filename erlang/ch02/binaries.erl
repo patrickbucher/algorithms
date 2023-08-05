35fe07d32827d4d0be2a20b55970183c8a95df0f
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-05 12:22:16 +0200

    Erlang: binary to decimal conversion

--- erlang/ch02/binaries.erl
+++ erlang/ch02/binaries.erl
@@ -1,10 +1,26 @@
 -module(binaries).
--export([add_binary/2]).
+-export([add_binary/2, binary_to_decimal/1]).
 
 add_binary([], []) -> [];
 add_binary(L, R) ->
     if length(L) =/= length(R) -> [];
-       true -> add_binary(L, R, [])
+       true -> add_binary(L, R, [], 0)
     end.
 
-add_binary(_, _, C) -> C.
+add_binary([], [], C, Carry) -> lists:reverse([Carry|C]);
+add_binary([HL|TL], [HR|TR], C, Carry) ->
+    Bit = HL + HR + Carry,
+    case Bit of
+        0 -> add_binary(TL, TR, [0|C], 0);
+        1 -> add_binary(TL, TR, [1|C], 0);
+        2 -> add_binary(TL, TR, [0|C], 1);
+        3 -> add_binary(TL, TR, [1|C], 1);
+        _ -> []
+    end.
+
+binary_to_decimal([]) -> 0;
+binary_to_decimal(Bin) -> binary_to_decimal(Bin, 0, 0).
+
+binary_to_decimal([], Acc, _) -> Acc;
+binary_to_decimal([H|T], Acc, I) ->
+    binary_to_decimal(T, Acc + H * round(math:pow(2, I)), I + 1).
--- erlang/ch02/binaries_tests.erl
+++ erlang/ch02/binaries_tests.erl
@@ -5,4 +5,21 @@ add_binary_test() ->
     ?assert(sorting:equal(binaries:add_binary([], []), [])),
     ?assert(sorting:equal(binaries:add_binary([0], []), [])),
     ?assert(sorting:equal(binaries:add_binary([], [0]), [])),
-    ?assert(sorting:equal(binaries:add_binary([0], [0]), [0, 0])).
+    ?assert(sorting:equal(binaries:add_binary([0], [0]), [0, 0])),
+    ?assert(sorting:equal(binaries:add_binary([0], [1]), [1, 0])),
+    ?assert(sorting:equal(binaries:add_binary([1], [0]), [1, 0])),
+    ?assert(sorting:equal(binaries:add_binary([1], [1]), [0, 1])),
+    ?assert(sorting:equal(binaries:add_binary([1, 1, 1], [0, 0, 1]), [1, 1, 0, 1])),
+    ?assert(sorting:equal(binaries:add_binary([1, 0, 1, 0], [0, 1, 0, 1]), [1, 1, 1, 1, 0])).
+
+binary_to_decimal_test() ->
+    ?assertEqual(binaries:binary_to_decimal([]), 0),
+    ?assertEqual(binaries:binary_to_decimal([0]), 0),
+    ?assertEqual(binaries:binary_to_decimal([1]), 1),
+    ?assertEqual(binaries:binary_to_decimal([0, 1]), 2),
+    ?assertEqual(binaries:binary_to_decimal([1, 1]), 3),
+    ?assertEqual(binaries:binary_to_decimal([0, 0, 1]), 4),
+    ?assertEqual(binaries:binary_to_decimal([1, 0, 1]), 5),
+    ?assertEqual(binaries:binary_to_decimal([0, 1, 1]), 6),
+    ?assertEqual(binaries:binary_to_decimal([1, 1, 1]), 7),
+    ?assertEqual(binaries:binary_to_decimal([0, 0, 0, 1]), 8).
