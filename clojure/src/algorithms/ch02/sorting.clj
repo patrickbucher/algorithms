6cb5a6f17d71c3f77ad8fd164b56194ab084afda
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-06 10:24:20 +0200

    Clojure: use vector for sequences of random numbers; is-sorted with tests

--- clojure/src/algorithms/ch02/sorting.clj
+++ clojure/src/algorithms/ch02/sorting.clj
@@ -1,8 +1,5 @@
 (ns algorithms.ch02.sorting)
 
-(defn hello [what]
-  (str "hello, " what))
-
 (defn equal
   "Returns true if the vectors are equal, false otherwise."
   [left right]
@@ -11,13 +8,17 @@
         (= (first left) (first right)) (equal (rest left) (rest right))
         :else false))
 
-(defn random-list
-  "Returns a list of size n with random elements in [min;max[."
-  ([n min max]
-   (if (>= min max)
-     []
-     (random-list n min max '())))
-  ([n min max acc]
-   (if (= n 0)
-     acc
-     (random-list (dec n) min max (cons (+ (rand-int (- max min)) min) acc)))))
+(defn random-vec
+  "Returns a vector of size n with random elements in [min;max[."
+  [n min max]
+  (if (>= min max)
+    []
+    (vec (take n (drop 1 (iterate (fn [_] (+ (rand-int (- max min)) min)) 0))))))
+
+(defn is-sorted
+  "Returns true if the given vector is sorted in order of the predicate
+  p, false otherwise."
+  [v p]
+  (if (or (empty? v) (= (count v) 1))
+    true
+    (and (p (first v) (second v)) (is-sorted (rest v) p))))
--- clojure/test/algorithms/ch02/sorting_test.clj
+++ clojure/test/algorithms/ch02/sorting_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [algorithms.ch02.sorting :refer :all]))
 
-(deftest some-test
-  (testing "everything works"
-    (is (= (hello "world") "hello, world"))))
-
 (deftest test-equal
   (testing "equality of two vectors"
     (is (equal [] []))
@@ -16,10 +12,29 @@
     (is (not (equal [0 2 1] [0 1 2])))
     (is (not (equal [0 1 2 3] [0 1 2])))))
 
-(deftest test-random-list
-  (testing "list of given size with random elements within limits"
-    (is (equal (random-list 0 1 10) []))
-    (is (equal (random-list 10 10 1) []))
-    (let [l (random-list 1000 1 10)]
+(deftest test-random-vec
+  (testing "vector of given size with random elements within limits"
+    (is (equal (random-vec 0 1 10) []))
+    (is (equal (random-vec 10 10 1) []))
+    (let [l (random-vec 1000 1 10)]
       (is (= (count l) 1000))
       (is (every? #(and (>= % 1) (< % 10)) l)))))
+
+(deftest test-is-sorted
+  (testing "ascending order of elements in a vector"
+    (let [asc #(<= %1 %2)
+          desc #(>= %1 %2)]
+      (is (is-sorted [] asc))
+      (is (is-sorted [] desc))
+      (is (is-sorted [0] asc))
+      (is (is-sorted [0] desc))
+      (is (is-sorted [0 1 2] asc))
+      (is (not (is-sorted [0 1 2] desc)))
+      (is (not (is-sorted [2 1 0] asc)))
+      (is (is-sorted [2 1 0] desc))
+      (is (is-sorted [0 1 2 3 4 5 6 7 8 9] asc))
+      (is (not (is-sorted [0 1 2 3 4 5 6 7 8 9] desc)))
+      (is (not (is-sorted [9 8 7 6 5 4 3 2 1 0] asc)))
+      (is (is-sorted [9 8 7 6 5 4 3 2 1 0] desc))
+      (is (not (is-sorted [5 4 6 3 7 2 1 8 9 0] asc)))
+      (is (not (is-sorted [5 4 6 3 7 2 1 8 9 0] desc))))))
