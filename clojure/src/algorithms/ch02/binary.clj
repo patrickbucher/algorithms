2ca4e9fa5c638447c1f979e57553365350874911
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-06 21:53:46 +0200

    Clojure: binary/decimal conversion

--- clojure/src/algorithms/ch02/binary.clj
+++ clojure/src/algorithms/ch02/binary.clj
@@ -1,12 +1,13 @@
-(ns algorithms.ch02.binary)
+(ns algorithms.ch02.binary
+  (:require [clojure.math :refer [pow]]))
 
 (defn add-binary
   "Adds two binary numbers represented as little endian collections of
   zeros and ones."
   ([as bs]
    (if (not (= (count as) (count bs)))
-             []
-             (add-binary as bs [] 0)))
+     []
+     (add-binary as bs [] 0)))
   ([as bs cs acc]
    (if (or (empty? as) (empty? bs))
      (conj cs acc)
@@ -18,3 +19,28 @@
        (cond (or (= c 0) (= c 1)) (add-binary as bs (conj cs c) 0)
              (= c 2) (add-binary as bs (conj cs 0) 1)
              (= c 3) (add-binary as bs (conj cs 1) 1))))))
+
+(defn binary-to-decimal
+  "Converts a little-endian binary number to its decimal representation."
+  ([bin] (if (empty? bin)
+           [0]
+           (binary-to-decimal bin 0 0)))
+  ([bin i acc]
+   (if (empty? bin)
+     acc
+     (let [b (first bin)
+           bin (rest bin)]
+       (recur bin (inc i) (+ acc (int (* b (pow 2 i)))))))))
+
+(defn decimal-to-binary
+  "Converts a decimal number to its little-endian binary represenation."
+  ([x] (if (<= x 1)
+         [0]
+         (decimal-to-binary x [])))
+  ([x acc]
+   (if (= x 0)
+     acc
+     (let [d (mod x 2)
+           x (int (/ x 2))
+           acc (conj acc d)]
+       (recur x acc)))))
--- clojure/test/algorithms/ch02/binary_test.clj
+++ clojure/test/algorithms/ch02/binary_test.clj
@@ -3,10 +3,34 @@
             [algorithms.ch02.binary :refer :all]
             [algorithms.ch02.sorting :refer :all]))
 
-
 (deftest test-add-binary
   (testing "adding little-endian binaries"
     (is (equal (add-binary [] []) [0]))
     (is (equal (add-binary [0] [1]) [1 0]))
     (is (equal (add-binary [0 1 0 1] [1 0 1 1]) [1 1 1 0 1]))
     (is (equal (add-binary [0 1 1 1] [1 0 1 1]) [1 1 0 1 1]))))
+
+(deftest test-binary-to-decimal
+  (testing "little-endian binary to decimal conversion"
+    (is (binary-to-decimal []) 0)
+    (is (binary-to-decimal [0]) 0)
+    (is (binary-to-decimal [1]) 1)
+    (is (binary-to-decimal [0 1]) 2)
+    (is (binary-to-decimal [1 1]) 3)
+    (is (binary-to-decimal [0 0 1]) 4)
+    (is (binary-to-decimal [1 0 1]) 5)
+    (is (binary-to-decimal [0 1 1]) 6)
+    (is (binary-to-decimal [1 1 1]) 7)
+    (is (binary-to-decimal [0 0 0 1]) 8)))
+
+(deftest test-decimal-to-binary
+  (testing "decimal to little-endian binary conversion"
+    (is (decimal-to-binary 0) [0])
+    (is (decimal-to-binary 1) [1])
+    (is (decimal-to-binary 2) [0 1])
+    (is (decimal-to-binary 3) [1 1])
+    (is (decimal-to-binary 4) [0 0 1])
+    (is (decimal-to-binary 5) [1 0 1])
+    (is (decimal-to-binary 6) [0 1 1])
+    (is (decimal-to-binary 7) [1 1 1])
+    (is (decimal-to-binary 8) [0 0 0 1])))
