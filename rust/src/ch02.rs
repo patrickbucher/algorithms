cd35c499312d2a519a0c7e139fe73bace96b41c1
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-07 15:27:12 +0200

    Rust: selection sort

--- rust/src/ch02.rs
+++ rust/src/ch02.rs
@@ -83,6 +83,30 @@ pub fn decimal_to_binary(mut dec: u32) -> Vec<u8> {
     return acc;
 }
 
+pub fn selection_sort<T: PartialOrd + Copy>(values: &Vec<T>) -> Vec<T> {
+    let n = values.len();
+    let mut ordered: Vec<T> = Vec::with_capacity(n);
+    for i in 0..n {
+        ordered.push(values[i]);
+    }
+    if n <= 1 {
+        return ordered;
+    }
+    for i in 0..(n - 1) {
+        let mut l = ordered[i];
+        let mut li = i;
+        for j in (i + 1)..n {
+            if ordered[j] < l {
+                l = ordered[j];
+                li = j;
+            }
+        }
+        ordered[li] = ordered[i];
+        ordered[i] = l;
+    }
+    return ordered;
+}
+
 #[cfg(test)]
 mod tests {
     use crate::ch02::add_binary;
@@ -90,9 +114,45 @@ mod tests {
     use crate::ch02::decimal_to_binary;
     use crate::ch02::insertion_sort;
     use crate::ch02::linear_search;
+    use crate::ch02::selection_sort;
     use crate::sorting::equal;
     use crate::sorting::is_sorted_asc;
     use crate::sorting::random_vec;
+
+    #[test]
+    fn test_selection_sort_empty_vec() {
+        let v: Vec<i32> = vec![];
+        assert!(equal(&v, &selection_sort(&v)));
+    }
+
+    #[test]
+    fn test_selection_sort_single_element() {
+        let v: Vec<i32> = vec![1];
+        assert!(equal(&v, &selection_sort(&v)));
+    }
+
+    #[test]
+    fn test_selection_sort_multiple_elements() {
+        let values = vec![3, 1, 2];
+        let expected = vec![1, 2, 3];
+        let actual = &selection_sort(&values);
+        assert!(equal(&expected, &actual));
+    }
+
+    #[test]
+    fn test_selection_sort_more_multiple_elements() {
+        let values = vec![6, 1, 2, 5, 4, 3];
+        let expected = vec![1, 2, 3, 4, 5, 6];
+        let actual = &selection_sort(&values);
+        assert!(equal(&expected, &actual));
+    }
+
+    #[test]
+    fn test_selection_sort_long_vector() {
+        let values = random_vec(1000, 1, 1000);
+        let actual = &selection_sort(&values);
+        assert!(is_sorted_asc(actual));
+    }
 
     #[test]
     fn test_decimal_to_binary() {
@@ -194,19 +254,19 @@ mod tests {
     }
 
     #[test]
-    fn test_sort_empty_vec() {
+    fn test_insertion_sort_empty_vec() {
         let v: Vec<i32> = vec![];
         assert!(equal(&v, &insertion_sort(&v)));
     }
 
     #[test]
-    fn test_sort_single_element() {
+    fn test_insertion_sort_single_element() {
         let v: Vec<i32> = vec![1];
         assert!(equal(&v, &insertion_sort(&v)));
     }
 
     #[test]
-    fn test_sort_multiple_elements() {
+    fn test_insertion_sort_multiple_elements() {
         let values = vec![3, 1, 2];
         let expected = vec![1, 2, 3];
         let actual = &insertion_sort(&values);
@@ -214,7 +274,7 @@ mod tests {
     }
 
     #[test]
-    fn test_sort_more_multiple_elements() {
+    fn test_insertion_sort_more_multiple_elements() {
         let values = vec![6, 1, 2, 5, 4, 3];
         let expected = vec![1, 2, 3, 4, 5, 6];
         let actual = &insertion_sort(&values);
@@ -222,7 +282,7 @@ mod tests {
     }
 
     #[test]
-    fn test_sort_long_vector() {
+    fn test_insertion_sort_long_vector() {
         let values = random_vec(1000, 1, 1000);
         let actual = &insertion_sort(&values);
         assert!(is_sorted_asc(actual));
