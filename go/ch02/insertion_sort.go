dddea75882c0db261ea66de9f524d0435444b361
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-09 22:06:18 +0200

    Go: recursive insertion sort

--- go/ch02/insertion_sort.go
+++ go/ch02/insertion_sort.go
@@ -21,3 +21,29 @@ func InsertionSort[T constraints.Ordered](slice []T) []T {
 	}
 	return ordered
 }
+
+// RecursiveInsertionSort performs Insertion Sort recursively.
+func RecursiveInsertionSort[T constraints.Ordered](slice []T) []T {
+	n := len(slice)
+	ordered := make([]T, n)
+	copy(ordered, slice)
+	if n <= 1 {
+		return ordered
+	}
+	recursiveInsertionSort(ordered, n)
+	return ordered
+}
+
+func recursiveInsertionSort[T constraints.Ordered](slice []T, n int) {
+	if n <= 1 {
+		return
+	}
+	recursiveInsertionSort(slice, n-1)
+	e := slice[n-1]
+	i := n - 2
+	for i >= 0 && slice[i] > e {
+		slice[i+1] = slice[i]
+		i--
+	}
+	slice[i+1] = e
+}
--- go/ch02/insertion_sort_test.go
+++ go/ch02/insertion_sort_test.go
@@ -12,18 +12,29 @@ type SortTestCase[T constraints.Ordered] struct {
 	Expected []T
 }
 
+var sortTests = []SortTestCase[int]{
+	{[]int{}, []int{}},
+	{[]int{1}, []int{1}},
+	{[]int{2, 1}, []int{1, 2}},
+	{[]int{3, 1, 2}, []int{1, 2, 3}},
+	{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+}
+
 func TestInsertionSort(t *testing.T) {
-	tests := []SortTestCase[int]{
-		{[]int{}, []int{}},
-		{[]int{1}, []int{1}},
-		{[]int{2, 1}, []int{1, 2}},
-		{[]int{3, 1, 2}, []int{1, 2, 3}},
-		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
-	}
-	for _, test := range tests {
+	for _, test := range sortTests {
 		actual := InsertionSort(test.Input)
 		if !sorting.Equal(actual, test.Expected) {
 			t.Errorf("expected InsertionSort(%v) to be %v, was %v",
+				test.Input, test.Expected, actual)
+		}
+	}
+}
+
+func TestRecursiveInsertionSort(t *testing.T) {
+	for _, test := range sortTests {
+		actual := RecursiveInsertionSort(test.Input)
+		if !sorting.Equal(actual, test.Expected) {
+			t.Errorf("expected RecursiveInsertionSort(%v) to be %v, was %v",
 				test.Input, test.Expected, actual)
 		}
 	}
