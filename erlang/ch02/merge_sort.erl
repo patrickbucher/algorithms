e6d0542525105c9f86b47aefc522d64799b0f11d
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-12 11:00:04 +0200

    Erlang: simplified merge sort API

--- erlang/ch02/merge_sort.erl
+++ erlang/ch02/merge_sort.erl
@@ -3,15 +3,11 @@
 
 sort([]) -> [];
 sort([H]) -> [H];
-sort(List) -> merge_sort(List).
-
-merge_sort([]) -> [];
-merge_sort([H]) -> [H];
-merge_sort(List) ->
+sort(List) ->
     N = length(List),
     Q = N div 2,
-    Left = merge_sort(lists:sublist(List, 1, Q)),
-    Right = merge_sort(lists:sublist(List, Q+1, N-Q)),
+    Left = sort(lists:sublist(List, 1, Q)),
+    Right = sort(lists:sublist(List, Q+1, N-Q)),
     merge(Left, Right, []).
 
 merge([], [], Acc) -> lists:reverse(Acc);
