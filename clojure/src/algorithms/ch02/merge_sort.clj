f26f8f953588cf84d6b1cb7754023b0e2348292b
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-13 09:25:57 +0200

    Clojure: Merge Sort

---
+++ clojure/src/algorithms/ch02/merge_sort.clj
@@ -0,0 +1,27 @@
+(ns algorithms.ch02.merge-sort)
+
+(defn merge-ordered
+  "Merges the given ordered vectors into a single ordered vector."
+  ([l r] (merge-ordered l r []))
+  ([l r acc]
+   (cond (and (empty? l) (empty? r)) acc
+         (empty? l) (flatten [acc r])
+         (empty? r) (flatten [acc l])
+         :else (let [a (first l)
+                     b (first r)
+                     nl (rest l)
+                     nr (rest r)]
+                 (if (<= a b)
+                   (recur nl r (conj acc a))
+                   (recur l nr (conj acc b)))))))
+
+(defn merge-sort
+  "Returns an ascendingly sorted vector."
+  [v]
+  (if (or (empty? v) (= (count v) 1))
+    v
+    (let [n (count v)
+          m (int (/ n 2))
+          l (merge-sort (subvec v 0 m))
+          r (merge-sort (subvec v m n))]
+      (merge-ordered l r))))
---
+++ clojure/test/algorithms/ch02/merge_sort_test.clj
@@ -0,0 +1,14 @@
+(ns algorithms.ch02.merge-sort-test
+  (:require [clojure.test :refer :all]
+            [algorithms.ch02.sorting :refer :all]
+            [algorithms.ch02.merge-sort :refer :all]))
+
+(deftest test-merge-sort
+  (testing "merge sort"
+    (is (is-sorted-asc (merge-sort [])))
+    (is (is-sorted-asc (merge-sort [1])))
+    (is (is-sorted-asc (merge-sort [1 2 3 4 5])))
+    (is (is-sorted-asc (merge-sort [5 1 3 2 4])))
+    (is (equal (merge-sort [9 1 8 2 7 3 6 4 5 0]) [0 1 2 3 4 5 6 7 8 9]))
+    (is (is-sorted-asc (merge-sort (random-vec 1000 0 1000))))))
+
