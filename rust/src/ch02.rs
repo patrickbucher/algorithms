5bf130ca9767a4c78db46cc13a8147e68c5082e2
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-14 09:29:41 +0200

    Rust: Bubble Sort

--- rust/src/ch02.rs
+++ rust/src/ch02.rs
@@ -334,11 +334,33 @@ fn insert_next<T: PartialOrd + Copy>(values: &mut Vec<T>, n: usize) {
     values[insert_at] = e;
 }
 
+pub fn bubble_sort<T: PartialOrd + Copy>(values: &Vec<T>) -> Vec<T> {
+    let n = values.len();
+    let mut ordered: Vec<T> = Vec::with_capacity(n);
+    for i in 0..n {
+        ordered.push(values[i]);
+    }
+    if n <= 1 {
+        return ordered;
+    }
+    for i in 0..n {
+        for j in ((i + 1)..n).rev() {
+            if ordered[j] < ordered[j - 1] {
+                let tmp = ordered[j];
+                ordered[j] = ordered[j - 1];
+                ordered[j - 1] = tmp;
+            }
+        }
+    }
+    return ordered;
+}
+
 #[cfg(test)]
 mod tests {
     use crate::ch02::add_binary;
     use crate::ch02::binary_search;
     use crate::ch02::binary_to_decimal;
+    use crate::ch02::bubble_sort;
     use crate::ch02::decimal_to_binary;
     use crate::ch02::insertion_sort;
     use crate::ch02::linear_search;
@@ -350,6 +372,41 @@ mod tests {
     use crate::sorting::is_sorted_asc;
     use crate::sorting::random_vec;
     use rand::Rng;
+
+    #[test]
+    fn test_bubble_sort_empty_vec() {
+        let v: Vec<i32> = vec![];
+        assert!(equal(&v, &bubble_sort(&v)));
+    }
+
+    #[test]
+    fn test_bubble_sort_single_element() {
+        let v: Vec<i32> = vec![1];
+        assert!(equal(&v, &bubble_sort(&v)));
+    }
+
+    #[test]
+    fn test_bubble_sort_multiple_elements() {
+        let values = vec![3, 1, 2];
+        let expected = vec![1, 2, 3];
+        let actual = &bubble_sort(&values);
+        assert!(equal(&expected, &actual));
+    }
+
+    #[test]
+    fn test_bubble_sort_more_multiple_elements() {
+        let values = vec![6, 1, 2, 5, 4, 3];
+        let expected = vec![1, 2, 3, 4, 5, 6];
+        let actual = &bubble_sort(&values);
+        assert!(equal(&expected, &actual));
+    }
+
+    #[test]
+    fn test_bubble_sort_long_vector() {
+        let values = random_vec(1000, 1, 1000);
+        let actual = &bubble_sort(&values);
+        assert!(is_sorted_asc(actual));
+    }
 
     #[test]
     fn test_recursive_insertion_sort_empty_vec() {
